b261f74bad4550f01ff98d525aee0ce2227d65d1
4HACker4 Gaming <134810317+4HACker4Gaming@users.noreply.github.com>
2024-07-18 18:25:06 +0100

    Update roulette.py

--- rus_roulette/roulette.py
+++ rus_roulette/roulette.py
@@ -1,11 +1,12 @@
 import random as rnd
 import sys
+import time
 
 sys.setrecursionlimit(100000)
 
 
 def roulette():
-    global lv, final, item_c, item_u, dev, store, showData, oneRound, higherDealerHealth, dmg, gm, player_hp, dealer_hp, blank_a, round_a
+    global lv, final, item_c, item_u, dev, store, showData, oneRound, higherDealerHealth, dmg, gm, player_hp, dealer_hp, blank_a, round_a, rapidfire, rapidfire_c
 
     print('Russian roulette in python')
 
@@ -19,6 +20,8 @@ def roulette():
     gm = 'Standard gamemode'
     dif = 2
     dif_s = 'normal'
+    rapidfire = ''
+    rapidfire_c = 0.5
 
     # Gamemode modifiers
     showData = True
@@ -26,6 +29,7 @@ def roulette():
     higherDealerHealth = False
     dmg = 1
     canDie = True
+    randomAfterShot = False
 
     # Custom
     scalePlayerHp = True
@@ -36,11 +40,16 @@ def roulette():
     it = '\033[3m'
     n = '\033[0m'
 
+    time.sleep(0.1)
     custom = input(f'{it}Custom? (y/n): {n}') == 'y'
+    time.sleep(0.1)
 
     def dif_gm():
-        global dev, dif, dif_s, showData, oneRound, higherDealerHealth, dmg, gm, canDie
+        global dev, dif, dif_s, showData, oneRound, higherDealerHealth
+        global dmg, gm, canDie, randomAfterShot
         canDie = input('Practice mode? (Become immortal) (y/n): ') != 'y'
+
+        time.sleep(0.1)
 
         dif_c = input(
             'DIFFUCULTY SELECT\nEnter: 0 for easy, 1 for hard, anything else for normal: '
@@ -70,9 +79,12 @@ def roulette():
             dif = 2
             dif_s = 'normal'
 
+        time.sleep(0.1)
+
         gm_c = input(
             '''\nGAMEMODE SELECT\nEnter 0 for standard, 1 for unknown stats, 2 for one round,
-3 for dual wield, 4 for high dealer health, or 5 for ALL of them: ''')
+      3 for dual wield, 4 for high dealer health, 5 for randomise bullets every shot,
+      or 6 for ALL of them: ''')
         if gm_c == '1':
             showData = False
             gm = 'Unknown stats gamemode'
@@ -86,17 +98,22 @@ def roulette():
             higherDealerHealth = True
             gm = 'High dealer health gamemode'
         elif gm_c == '5':
+            randomAfterShot = True
+            gm = 'Randomise bullets every shot gamemode'
+        elif gm_c == '6':
             gm = 'ALL gamemode'
             showData = False
             oneRound = True
             dmg = 2
             higherDealerHealth = True
 
+        time.sleep(0.1)
+
     def custom_setup():
         global roundMin, roundMax, blankMin, blankMax  # Bullets
         global finalRoundMin, finalRoundMax, finalBlankMin, finalBlankMax  # Bullets (Final)
         global scalePlayerHp, scaleDealerHp, scaleHealthAmountP, scaleHealthAmountD  # Hp scale
-        global gm, canDie  # Other
+        global gm  # Other
         gm = 'Custom'
 
         roundMin = int(input('Set round min: '))
@@ -115,16 +132,15 @@ def roulette():
         if scaleDealerHp:
             scaleHealthAmountD = int(input('Set dealer health scale amount: '))
 
-        canDie = input('Become immortal? (y/n): ') != 'y'
-
-    dif_gm() if not custom else custom_setup()
+    custom_setup() if custom else None
+    dif_gm()
 
     hints = [
         f'{it}Developer mode?{n}', f'{it}Dying is bad, don\'t die{n}',
         f'{it}Dying is fine, just try again.{n}', f'{it}Hard != easy{n}',
         f'{it}Normal = rnd.randint(){n}', f'{it}Easy != hard{n}',
         f'{it}[RESET TO CHECKPOINT]{n}',
-        f'{it}1. Enable cheats on hard 2. "health" 3. "round" 4. "YOU"{n}'
+        f'{it}1. Enable cheats on hard 2. "+health" 3. "+round" 4. "YOU"{n}'
     ]
 
     def prep():
@@ -165,20 +181,28 @@ def roulette():
             blank_a = rnd.randint(3, 5)
 
     def main():
-        global lv, item_c
+        global lv, item_c, rapidfire, rapidfire_c
+
+        rapidfire = ''
+        rapidfire_c = 0.5
         lv_d = lv if not final else 'FINAL'
         prep()
         prepb()
+        time.sleep(0.1)
         print(f'Level {lv_d} ({gm})')
+        time.sleep(0.1)
         round()
 
         if dealer_hp == 0:
             print(f'YOU WIN LEVEL {lv_d} ({gm})!')
             lv += 1
             item_c += 1
+            time.sleep(0.1)
         else:
             print('LOSE. TRY AGAIN')
+            time.sleep(0.1)
             print(rnd.choice(hints))
+            time.sleep(0.1)
 
         if lv == 6 or lv == 7:
             return
@@ -193,8 +217,12 @@ def roulette():
             return
 
         if blank_a == 0 and round_a == 0:
+            print('Reloading...')
+            time.sleep(0.5)
             print('GUN RELOADED')
             prepb()
+
+        prepb() if randomAfterShot else None
 
         if blank_a or round_a != 0:
             type_p = blank_a / round_a
@@ -210,7 +238,9 @@ def roulette():
 
         print(
             f'''Your health: {player_hp}\nDealer health: {dealer_hp}\n{blank_a} blank(s). 
-{round_a} round(s).''') if showData else print('STATS OFF (Unknown stats on)')
+    {round_a} round(s).''') if showData else print('STATS OFF (Unknown stats on)')
+
+        time.sleep(0.25)
 
         choice = input(f'Shoot YOU or DEALER or use ITEM({item_c}): ')
         if choice == 'YOU':
@@ -239,6 +269,7 @@ def roulette():
                 item_u = True
                 store = type
                 print(f'\n{it}The next bullet is a {store}{n}')
+                time.sleep(0.1)
                 round()
         elif choice == '+item' and dev:
             item_c += 1000000
@@ -247,29 +278,34 @@ def roulette():
             dealer_hp = 0
             print('instakill')
             round()
-        elif choice == 'health' and dev:
+        elif choice == '+health' and dev:
             dealer_hp = 9999
             player_hp = 9999
             round()
-        elif choice == 'round' and dev:
+        elif choice == '+round' and dev:
             blank_a = 0
             round_a = 9999
             round()
         else:
+            time.sleep(0.1)
             print('\nUNKNOWN ACTION\nuse "YOU" or "DEALER" or "ITEM"\n')
 
         round()
 
     def d_round():
         print()
-        global player_hp, dealer_hp, blank_a, round_a
+        global player_hp, dealer_hp, blank_a, round_a, rapidfire, rapidfire_c
 
         if player_hp == 0 or dealer_hp == 0:
             return
 
         if blank_a == 0 and round_a == 0:
+            print('Reloading...')
+            time.sleep(0.5)
             print('GUN RELOADED')
             prepb()
+
+        prepb() if randomAfterShot else None
 
         if blank_a or round_a != 0:
             type_p = blank_a / round_a
@@ -287,16 +323,22 @@ def roulette():
         else:
             choice = 'self' if rnd.randint(0, 1) > 0.5 else 'player'
 
+        time.sleep(rapidfire_c)
+        rapidfire_c -= 0.1 if choice == rapidfire and rapidfire_c > 0.2 else 0
+
         if choice == 'self':
             print('DEALER SHOOT SELF')
             if type == 'round':
                 dealer_hp -= dmg
                 round_a = round_a - 1
                 print(f'ROUND. DEALER -{dmg} HP')
+                rapidfire = ''
+                rapidfire_c = 0.5
                 round()
             elif type == 'blank':
                 blank_a = blank_a - 1
                 print('BLANK. DEALER TURN')
+                rapidfire = 'self'
                 d_round()
         elif choice == 'player':
             print('DEALER SHOOT PLAYER')
@@ -304,10 +346,13 @@ def roulette():
                 player_hp -= dmg if canDie else 0
                 round_a = round_a - 1
                 print(f'ROUND. -{dmg} HP')
+                rapidfire = 'player'
                 d_round()
             elif type == 'blank':
                 blank_a = blank_a - 1
                 print('BLANK. YOUR TURN')
+                rapidfire = ''
+                rapidfire_c = 0.5
                 round()
 
     main()
@@ -316,8 +361,11 @@ def roulette():
 
     main()
 
+    time.sleep(0.1)
     print(f'CONGRAGULATIONS! YOU WIN!\nOn {dif_s} difficulty... ({gm})')
+    time.sleep(0.1)
     if not canDie:
         print('Now try without immortality!')
     elif dev:
         print('Now try without dev mode on!')
+
